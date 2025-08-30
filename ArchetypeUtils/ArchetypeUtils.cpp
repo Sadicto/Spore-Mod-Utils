48e4a511fae21a51f8b10a675d140e371891f6d8
Sad <Saidicto@gmail.com>
2025-08-29 23:27:20 -0300

    Add ArchetypeToString

--- ArchetypeUtils/ArchetypeUtils.cpp
+++ ArchetypeUtils/ArchetypeUtils.cpp
@@ -31,5 +31,22 @@ namespace SporeModUtils {
                 return archetype;
             }
         }
+
+        eastl::string16 ArchetypeToString(Simulator::Archetypes archetype) {
+            switch (archetype) {
+            case Simulator::Archetypes::kArchetypeWarrior: case Simulator::Archetypes::kArchetypePlayerWarrior: return u"Warrior";
+            case Simulator::Archetypes::kArchetypeTrader: case Simulator::Archetypes::kArchetypePlayerTrader: return u"Trader";
+            case Simulator::Archetypes::kArchetypeScientist: case Simulator::Archetypes::kArchetypePlayerScientist: return u"Scientist";
+            case Simulator::Archetypes::kArchetypeShaman: case Simulator::Archetypes::kArchetypePlayerShaman: return u"Shaman";
+            case Simulator::Archetypes::kArchetypeBard: case Simulator::Archetypes::kArchetypePlayerBard: return u"Bard";
+            case Simulator::Archetypes::kArchetypeZealot: case Simulator::Archetypes::kArchetypePlayerZealot: return u"Zealot";
+            case Simulator::Archetypes::kArchetypeDiplomat: case Simulator::Archetypes::kArchetypePlayerDiplomat: return u"Diplomat";
+            case Simulator::Archetypes::kArchetypeEcologist: case Simulator::Archetypes::kArchetypePlayerEcologist: return u"Ecologist";
+            case Simulator::Archetypes::kArchetypeGrob: return u"Grob";
+            case Simulator::Archetypes::kArchetypePlayerWanderer: return u"Wanderer";
+            case Simulator::Archetypes::kArchetypePlayerKnight: return u"Knight";
+            default: return u"Unknown";
+            }
+        }
     }
 }
--- ArchetypeUtils/ArchetypeUtils.h
+++ ArchetypeUtils/ArchetypeUtils.h
@@ -2,13 +2,17 @@
 #include <Spore/Simulator/cStarRecord.h>
 namespace SporeModUtils {
     namespace ArchetypeUtils {
-        /**
-         * @brief Returns the base (non-player) archetype corresponding to a given archetype value.
-         * @param archetype The input value from the Archetypes enum.
-         * @return The corresponding base archetype if input is a player archetype,
-         * or the original value if no mapping is necessary.
-         */
+
+        /// @brief Returns the base (non-player) archetype corresponding to a given archetype value.
+        /// @param archetype The input value from the Archetypes enum.
+        /// @return The corresponding base archetype if input is a player archetype,
+        /// or the original value if no mapping is necessary.
         Simulator::Archetypes GetBaseArchetype(Simulator::Archetypes archetype);
+
+        /// @brief Converts an empire archetype enum value to its string representation.
+        /// @param archetype
+        /// @return The name of the archetype as a eastl::string16
+        eastl::string16 ArchetypeToString(Simulator::Archetypes archetype);
     }
 }
 
